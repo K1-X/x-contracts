39929afbcc259cd50d13ae3b5fd89edb989000fb
ethansmithone <ethansmithone@hotmail.com>
2021-04-15 19:22:35 +0800

    update Burnable

--- Burnable.sol
+++ Burnable.sol
@@ -10,20 +10,23 @@ contract Burnable {
         _;
     }
 
-modifier onlyBurner() {
+    modifier onlyBurner() {
         require(msg.sender == _burner, "caller is not a burner");
         _;
     }
- modifier onlyPendingBurner() {
+
+    modifier onlyPendingBurner() {
         require(msg.sender == pendingBurner);
         _;
     }
-   event BurnerTransferred(address indexed previousBurner, address indexed newBurner);
+
+    event BurnerTransferred(address indexed previousBurner, address indexed newBurner);
 
     constructor () internal {
         _burnallow = true;
         _burner = msg.sender;
     }
+
     function burnallow() public view returns (bool) {
         return _burnallow;
     }
@@ -32,11 +35,17 @@ modifier onlyBurner() {
         return _burner;
     }
 
-   function burnTrigger() public onlyBurner {
+    function burnTrigger() public onlyBurner {
         _burnallow = !_burnallow;
     }
 
-   function transferWhitelistAdmin(address newBurner) public onlyBurner {
+    function transferWhitelistAdmin(address newBurner) public onlyBurner {
         pendingBurner = newBurner;
+    }
+
+    function claimBurner() public onlyPendingBurner {
+        emit BurnerTransferred(_burner, pendingBurner);
+        _burner = pendingBurner;
+        pendingBurner = address(0);
     }
 }
