f02030649f5a146b2c1f074029218c1d181b2f58
michaelhenry122 <michaelhenry122@hotmail.com>
2021-04-16 09:50:52 +0800

    define data struct

--- Meltable.sol
+++ Meltable.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.5.11;
 
 contract Meltable {
-    
+  
+   mapping (address => bool) private _melters;
+    address private _melteradmin;
+    address public pendingMelterAdmin;    
 }
