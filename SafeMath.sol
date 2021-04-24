d4fde7b01d2bb736840ff22cff6348236e68d6ca
michaelhenry122 <michaelhenry122@hotmail.com>
2021-04-24 20:32:47 +0800

    div

--- SafeMath.sol
+++ SafeMath.sol
@@ -26,4 +26,16 @@ library SafeMath {
 
         return c;
     }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
 }
