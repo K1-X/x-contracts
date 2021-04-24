88edadf4f78a560e6d1fc14040bf3b36660e0f6b
michaelhenry122 <michaelhenry122@hotmail.com>
2021-04-24 20:38:18 +0800

    define attr

--- DIFSToken.sol
+++ DIFSToken.sol
@@ -10,5 +10,33 @@ import './Meltable.sol';
 import "./Rules.sol";
 
 contract DifsToken is AccountFrozenBalances, Ownable, Whitelisted, Burnable, Pausable, Mintable, Meltable {
-    
+    using SafeMath for uint256;
+    using Rules for Rules.Rule;
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupplyLimit;
+
+
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+    uint256 private _totalSupply;
+
+    enum RoleType { Invalid, FUNDER, DEVELOPER, MARKETER, COMMUNITY, SEED }
+
+    struct FreezeData {
+        address addr;
+        uint256 lastFreezeBlock;
+    }
+
+    mapping (address => RoleType) private _roles;
+    mapping (uint256 => Rules.Rule) private _rules;
+    mapping (address => FreezeData) private _freeze_datas;
+    uint256 public monthIntervalBlock = 172800;    
+    uint256 public yearIntervalBlock = 2102400;    
+
+    bool public seedPause = true;               
+
+	     
 }
